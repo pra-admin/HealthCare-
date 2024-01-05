c893588ce82a34e8566cae8dd8eb47bcb0874966
pra-admin <prajwal3741@gmail.com>
2024-01-05 18:31:21 +0530

    Patient module done by prajwal

--- HealthCare/src/test/java/com/HealthCare/HealthCareApplicationTests.java
+++ HealthCare/src/test/java/com/HealthCare/HealthCareApplicationTests.java
@@ -1,13 +1,18 @@
 package com.HealthCare;
 
+import com.HealthCare.entity.Patient;
+import com.HealthCare.repository.PatientRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HealthCareApplicationTests {
 
+
 	@Test
 	void contextLoads() {
+
 	}
 
 }
